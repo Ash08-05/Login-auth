a8eb95cff6e2eb09cc06b9c9e8aff16a3d7a7b0f
Ashmeet Band <bandashmeet07@gmail.com>
2026-01-28 09:54:27 +0530

    Update backend

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -144,7 +144,7 @@ export const logout = async (req, res) => {
 export const sendVerifyOtp = async (req, res) => {
   try {
 
-    const {userId} = req.body;
+    const userId = req.userId;
     const user = await userModel.findById(userId);
     if (user.isAccountVerified) {
       return res.json({ success: false, message: "Account Already verified" })
